a8fb19c1f9c28883a46804d157ca75e032ddb5c3
Chintan Radia <radiachints1512@gmail.com>
2017-10-03 23:26:11 +0530

    minor tweak
    Fixed extra white space

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
           <div className="Logo-header">
             <img src={headerBackground} className="App-logo-back" alt="logo-back" />
             <img src={logo} className="App-logo" alt="logo" />
-	        </div>
+          </div>
           <h1 className="App-title">Learn React in Hacktoberfest 2017 </h1>
         </header>
         <Main />
