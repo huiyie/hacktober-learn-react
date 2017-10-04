943b3e00296740dfdd4bda90473dd00ea5e78397
lunzhang <lun.zhang1993@gmail.com>
2017-10-04 17:19:23 -0400

    added basic routing

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import App from './App';
+import LoginForm from './components/LoginForm';
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <div>
+      <div id="routing-tab">
+         <div className="route"><Link to="/">Main</Link></div>
+         <div className="route"><Link to="/login">Login</Link></div>
+      </div>
+      <Route exact path="/" component={App} />
+      <Route exact path="/login" component={LoginForm} />
+    </div>
+  </Router>,
+  document.getElementById('root'));
 registerServiceWorker();
